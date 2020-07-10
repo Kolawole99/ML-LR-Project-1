fcac2ee09baf5383ff3657a452ed60423f65a6ca
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-10 03:15:40 +0100

    Evaluation for the model, Jaccard Index and Comgusion Matrix

--- LR.py
+++ LR.py
@@ -6,10 +6,14 @@ import pylab as pl
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
+import itertools
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import log_loss
 # %matplotlib inline #needed in jupyter notebooks
 
 #==================Loading the data into the project=================================
@@ -74,4 +78,62 @@ yhat_prob
 print(yhat_prob)
 
 
-#=========================
+#============================================EVALUATION========================================
+
+#===========================================Jaccard index========================================
+jaccard_similarity_score = jaccard_score(yhat, y_test)
+print("Jacccard Similarity Score: ", jaccard_similarity_score)
+
+
+#=========================================Confusion Matrix======================================
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+print(confusion_matrix(y_test, yhat, labels=[1,0]))
+
+#=======================plotting the confusion matrix
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, yhat, labels=[1,0])
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['churn=1','churn=0'],normalize= False,  title='Confusion matrix')
+
+#Report result and comparisons
+print (classification_report(y_test, yhat))
+    #Precision is a measure of the accuracy provided that a class label has been predicted. It is defined by: precision = TP / (TP + FP)
+    #Recall is true positive rate. It is defined as: Recall =  TP / (TP + FN)
+    #F1 score reaches its best value at 1 (perfect precision and recall) and worst at 0.
+
+
+
+
+
