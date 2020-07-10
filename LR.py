9c6343f612ba3c863633d4c7c4646cc89c758a1a
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-10 02:21:20 +0100

    Dataset file and reading it into a dataframe for use in the project

--- LR.py
+++ LR.py
@@ -1,7 +1,15 @@
+#===========================IMPORTING TREQUIRED LIBRARIES AND DATASETS==========================
+
+#===========================importing libraries, modules and packages======================
 import pandas as pd
 import pylab as pl
 import numpy as np
 import scipy.optimize as opt
 from sklearn import preprocessing
-%matplotlib inline 
 import matplotlib.pyplot as plt
+# %matplotlib inline #needed in jupyter notebooks
+
+#==================Loading the data into the project=================================
+churn_df = pd.read_csv("ChurnData.csv")
+data = churn_df.head()
+print(data)
